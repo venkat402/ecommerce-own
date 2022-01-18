c224524abdc754609ed34fcd750aa51499a3b9ed
Venkateswararao Adapala (MINDTREE LIMITED) <v-vadapala@microsoft.com>
2022-01-18 23:33:16 +0530

    Fixed few issues regarding product detail page

--- product/views.py
+++ product/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Product
+from reviews.models import Review
 
 
 # Create your views here.
@@ -7,11 +8,15 @@ from .models import Product
 
 def product_details(request, slug):
     product = None
+    reviews = None
     try:
         product = Product.objects.get(slug=slug)
+        reviews = Review.objects.select_related('user').select_related(
+            'product').select_related('user__profile').filter(product__id=product.id)
     except Exception:
         pass
     context = {
-        'product': product
+        'product': product,
+        'reviews': reviews
     }
     return render(request, 'product-detail.html', context)
